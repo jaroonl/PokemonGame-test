e2191ad4d1d44a69159f24aecd87e60766d4251b
jaroonl <jaronslin@gmail.com>
2023-07-20 14:52:23 -0700

    added new search bar and random pokemon generating

--- gameCode.js
+++ gameCode.js
@@ -1,4 +1,4 @@
-const list = [
+const pokemonNames = [
   "Bulbasaur",
   "Ivysaur",
   "Venusaur",
@@ -21,38 +21,199 @@ const list = [
   "Raticate",
   "Spearow",
   "Fearow",
+  "Ekans",
+  "Arbok",
+  "Pikachu",
+  "Raichu",
+  "Sandshrew",
+  "Sandslash",
+  "Nidoran♀",
+  "Nidorina",
+  "Nidoqueen",
+  "Nidoran♂",
+  "Nidorino",
+  "Nidoking",
+  "Clefairy",
+  "Clefable",
+  "Vulpix",
+  "Ninetales",
+  "Jigglypuff",
+  "Wigglytuff",
+  "Zubat",
+  "Golbat",
+  "Oddish",
+  "Gloom",
+  "Vileplume",
+  "Paras",
+  "Parasect",
+  "Venonat",
+  "Venomoth",
+  "Diglett",
+  "Dugtrio",
+  "Meowth",
+  "Persian",
+  "Psyduck",
+  "Golduck",
+  "Mankey",
+  "Primeape",
+  "Growlithe",
+  "Arcanine",
+  "Poliwag",
+  "Poliwhirl",
+  "Poliwrath",
+  "Abra",
+  "Kadabra",
+  "Alakazam",
+  "Machop",
+  "Machoke",
+  "Machamp",
+  "Bellsprout",
+  "Weepinbell",
+  "Victreebel",
+  "Tentacool",
+  "Tentacruel",
+  "Geodude",
+  "Graveler",
+  "Golem",
+  "Ponyta",
+  "Rapidash",
+  "Slowpoke",
+  "Slowbro",
+  "Magnemite",
+  "Magneton",
+  "Farfetch'd",
+  "Doduo",
+  "Dodrio",
+  "Seel",
+  "Dewgong",
+  "Grimer",
+  "Muk",
+  "Shellder",
+  "Cloyster",
+  "Gastly",
+  "Haunter",
+  "Gengar",
+  "Onix",
+  "Drowzee",
+  "Hypno",
+  "Krabby",
+  "Kingler",
+  "Voltorb",
+  "Electrode",
+  "Exeggcute",
+  "Exeggutor",
+  "Cubone",
+  "Marowak",
+  "Hitmonlee",
+  "Hitmonchan",
+  "Lickitung",
+  "Koffing",
+  "Weezing",
+  "Rhyhorn",
+  "Rhydon",
+  "Chansey",
+  "Tangela",
+  "Kangaskhan",
+  "Horsea",
+  "Seadra",
+  "Goldeen",
+  "Seaking",
+  "Staryu",
+  "Starmie",
+  "Mr. Mime",
+  "Scyther",
+  "Jynx",
+  "Electabuzz",
+  "Magmar",
+  "Pinsir",
+  "Tauros",
+  "Magikarp",
+  "Gyarados",
+  "Lapras",
+  "Ditto",
+  "Eevee",
+  "Vaporeon",
+  "Jolteon",
+  "Flareon",
+  "Porygon",
+  "Omanyte",
+  "Omastar",
+  "Kabuto",
+  "Kabutops",
+  "Aerodactyl",
+  "Snorlax",
+  "Articuno",
+  "Zapdos",
+  "Moltres",
+  "Dratini",
+  "Dragonair",
+  "Dragonite",
+  "Mewtwo",
+  "Mew",
 ];
 
-const output = document.querySelector(".output");
-const search = document.querySelector(".filter-input");
+const resultBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
 
-window.addEventListener("DOMContentLoaded", loadList);
-search.addEventListener("input", filter);
-
-function loadList() {
-  let temp = `<ul class="list-items">`;
-  list.forEach((item) => {
-    temp += `<li class="list-item"> ${item} </li>`;
-  });
-  temp += `</ul>`;
-}
-
-function filter(e) {
-  let temp = "";
-  const result = list.filter((item) =>
-    item.toLowerCase().includes(e.target.value.toLowerCase())
-  );
-  console.log(result);
-
-  if (result.length > 0) {
-    temp = `<ul class="list-items">`;
-    result.forEach((item) => {
-      temp += `<li class="list-item"> ${item} </li>`;
+inputBox.onkeyup = function () {
+  let result = [];
+  let input = inputBox.value;
+  if (input.length) {
+    result = pokemonNames.filter((keyword) => {
+      return keyword.toLowerCase().includes(input.toLowerCase());
     });
-    temp += `</ul>`;
-  } else {
-    temp = `<div class="no-item"> No Pokémon Found </div>`;
+    console.log(result);
   }
+  display(result);
 
-  output.innerHTML = temp;
+  if (!result.length) {
+    resultBox.innerHTML = "";
+  }
+};
+
+function display(result) {
+  const content = result.map((list) => {
+    return "<li onclick=selectInput(this)>" + list + "</li>";
+  });
+
+  resultBox.innerHTML = "<ul>" + content.join("") + "</ul>";
 }
+
+function selectInput(list) {
+  inputBox.value = list.innerHTML;
+  resultBox.innerHTML = "";
+}
+
+function generatePokemonImage() {
+  const numberOfPokemon = 151;
+  const randomImageNumber = Math.floor(Math.random() * numberOfPokemon) + 1;
+  const randomImageFilename = `pokemon/${randomImageNumber}.png`;
+
+  const imgElement = document.getElementById("randomImage");
+  imgElement.src = randomImageFilename;
+  imgElement.setAttribute("alt", `${randomImageNumber}`);
+
+  return randomImageNumber;
+}
+
+function checkUserInput() {
+  const userInput = document.getElementById("userInput").value;
+  const pokemonAtIndex = pokemonNames[randomImageNumber];
+
+  /*if (userInput.toLowerCase() === pokemonAtIndex.toLowerCase()) {
+    alert(
+      `Congratulations! You guessed correctly. The Pokemon number ${
+        randomPokemonNumber + 1
+      } is "${pokemonAtIndex}".`
+    );
+  } else {
+    alert(
+      `Oops! Incorrect guess. The Pokemon number ${
+        randomPokemonNumber + 1
+      } is "${pokemonAtIndex}".`
+    );
+  }*/
+}
+
+generatePokemonImage();
+checkUserInput();
